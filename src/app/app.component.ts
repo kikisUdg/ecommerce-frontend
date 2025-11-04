57138285f13c8bc021df1349be66d37089b57ca6
Luis Enrique Ricaño Patiño <luis.ricano@udgvirtual.udg.mx>
2025-11-04 00:41:23 -0600

    Sprint 1 final: integración de AuthService y ajustes de UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // importante mantener standalone
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ecommerce-frontend';
+  backendStatus = '';
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.health().subscribe({
+      next: (res) => {
+        console.log('Health endpoint:', res);
+        this.backendStatus = `${res.framework} está funcionando correctamente`;
+      },
+      error: (err) => {
+        console.error('Error al conectar con backend:', err);
+        this.backendStatus = 'Error al conectar con el servidor';
+      }
+    });
+  }
 }
